bd76c45daead7ed0ee6cb6fa4369b54a8f3c9bfe
msulaev <ewrewr>
2022-08-07 19:54:47 +0300

    add test for pricing

---
+++ src/test/java/com/github/PricingTest.java
@@ -0,0 +1,20 @@
+package com.github;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PricingTest {
+
+    @Test
+    public void shouldOpenPricingPlan(){
+        open("https://github.com");
+        $(byText("Pricing")).hover();
+        $(byText("Compare plans")).click();
+        $(".font-mktg").shouldHave(text("Choose the plan that’s right for you."));
+    }
+}
