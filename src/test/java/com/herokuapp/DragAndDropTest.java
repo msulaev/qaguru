a7c5b4cdf2844b9a48011306a1c3d837620648f1
msulaev <ewrewr>
2022-08-07 20:28:28 +0300

    add snippet and drag and drop tests

--- src/test/java/com/herokuapp/DragAndDropTest.java
+++ src/test/java/com/herokuapp/DragAndDropTest.java
@@ -1,2 +1,33 @@
-package com.herokuapp;public class DragAndDropTest {
+package com.herokuapp;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    static void setUp(){
+        Configuration.holdBrowserOpen = true;
+        Configuration.headless = false;
+    }
+
+    @Test
+    public void dragAndDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    public void adotherDragAndDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
 }
