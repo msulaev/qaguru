2afc7305a3e55a3fb008ada746264cbb3b5adffe
msulaev <ewrewr>
2022-08-20 17:56:07 +0300

    add homework

---
+++ src/test/java/guru/qa/domain/Root.java
@@ -0,0 +1,12 @@
+package guru.qa.domain;
+
+import java.util.ArrayList;
+
+public class Root {
+    public String id;
+    public String type;
+    public String name;
+    public double ppu;
+    public Batters batters;
+    public ArrayList<Topping> topping;
+}
