b8654bea8409ef7b02c11558097a0af53eeab729
msulaev <ewrewr>
2022-11-17 15:54:06 +0200

    Add allure @Stesp

--- src/main/java/pages/FormPage.java
+++ src/main/java/pages/FormPage.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import data.User;
 import elements.CalendarElement;
+import io.qameta.allure.Step;
 
 import java.io.File;
 import java.util.List;
@@ -31,6 +32,7 @@ public class FormPage {
     private final CalendarElement calendarElement = new CalendarElement();
     ElementsCollection cities = $$(".css-11unzgr div");
 
+    @Step("Open URL:https://demoqa.com/automation-practice-form")
     public FormPage open() {
         Selenide.open("https://demoqa.com/automation-practice-form");
         executeJavaScript("$('footer').remove()");
@@ -38,6 +40,7 @@ public class FormPage {
         return this;
     }
 
+    @Step("Check Result from with our User: {0}")
     public void checkResultsForm(User user) {
         resultModal.shouldHave(
                 text(user.getSubject()),
@@ -52,33 +55,39 @@ public class FormPage {
                 text(user.getBirthMonth()));
     }
 
+    @Step("Check that result modal not exist")
     public void checkThatResultModalNotExist() {
         resultModal.shouldNot(exist);
     }
 
+    @Step("Click to submit")
     public FormPage clickToSubmit() {
         submitBtn.click();
         return this;
     }
 
+    @Step("Upload image from resources")
     public FormPage uploadPicture() {
         uploadPicture.uploadFile(new File("src/test/resources/token_1.png"));
         return this;
 
     }
 
+    @Step("Click hobby")
     public FormPage checkHobby(String hobby) {
         $("#hobbiesWrapper").$(byText(hobby)).click();
         return this;
 
     }
 
+    @Step("Set subjects: {0}")
     public FormPage setSubject(String subj) {
         subject.setValue(subj).pressEnter();
         return this;
 
     }
 
+    @Step("Set date: {0}/{1}/{3}")
     public FormPage setBirthday(String birthDay, String birthYear, String birthMonth) {
         dateOfBirthInpt.click();
         calendarElement.setDate(birthDay, birthYear, birthMonth);
@@ -86,12 +95,14 @@ public class FormPage {
 
     }
 
+    @Step("Set number")
     public FormPage setPhoneNumber(String phoneNumber) {
         userNumber.setValue(phoneNumber);
         return this;
 
     }
 
+    @Step("Set gender: {0}")
     public FormPage checkGender(String gender) {
         if (gender == null) {
             return this;
@@ -102,34 +113,39 @@ public class FormPage {
 
     }
 
+    @Step("Set email: {0}")
     public FormPage setEmail(String email) {
         userEmail.setValue(email);
         return this;
     }
 
+    @Step("Set lastname: {0}")
     public FormPage setLastName(String surname) {
         lastName.setValue(surname);
         return this;
-
     }
 
+    @Step("Set firstname: {0}")
     public FormPage setFirstName(String name) {
         firstName.setValue(name);
         return this;
 
     }
 
+    @Step("Set state: {0}")
     public FormPage setState(String stateName) {
         state.click();
         $(byText(stateName)).click();
         return this;
     }
 
+    @Step("Set current adress: {0}")
     public FormPage setCurrentAddress(String address) {
         currentAddress.setValue(address);
         return this;
     }
 
+    @Step("Set city: {0}")
     public FormPage setCity(String cityName) {
         city.click();
         $(byText(cityName)).click();
