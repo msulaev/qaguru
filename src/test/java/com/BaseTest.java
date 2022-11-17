e0f4f06ecc3d0d943f6e8a266fc673d74949e411
msulaev <ewrewr>
2022-11-17 17:51:25 +0200

    Add system properties

--- src/test/java/com/BaseTest.java
+++ src/test/java/com/BaseTest.java
@@ -3,27 +3,36 @@ package com;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import io.qameta.allure.selenide.LogType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import utils.Attach;
 
+import java.util.logging.Level;
+
+
+
 public class BaseTest {
 
     @BeforeAll
     static void configure() {
-  //      SelenideLogger.addListener("AllureSelenide", new AllureSelenide().);
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+                .enableLogs(LogType.BROWSER, Level.ALL)
+        );
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", "chrome");
-        capabilities.setCapability("browserVersion", "100.0");
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
+        capabilities.setCapability("browserName", System.getProperty("browse", "chrome"));
+        capabilities.setCapability("browserVersion", System.getProperty("version", "100.0"));
+        capabilities.setCapability("enableVNC", System.getProperty("enableVNC"));
+        capabilities.setCapability("enableVideo", System.getProperty("enableVideo"));
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = System.getProperty("resolution");
+        Configuration.remote = System.getProperty("remote");
     }
 
     @AfterEach
@@ -31,6 +40,8 @@ public class BaseTest {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo();
+        if ((System.getProperty("enableVideo") != null)) {
+            Attach.addVideo();
+        }
     }
 }
